f2313dab4c024a7bd4e08c7cc64569c8e6bc0ce4
oluye7 <oluye7@naver.com>
2022-08-12 02:32:54 +0900

    디렉토리 구분 Splash 화면 추가 앱아이콘 추가 primary 칼라셋 추가

---
+++ AppSearcher/Presentation/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  AppSearcher
+//
+//  Created by 정동천 on 2022/08/12.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .primary
+    }
+}
---
+++ AppSearcher/Util/Extension++.swift
@@ -0,0 +1,12 @@
+//
+//  Extension++.swift
+//  AppSearcher
+//
+//  Created by 정동천 on 2022/08/12.
+//
+
+import UIKit
+
+extension UIColor {
+    class var primary: UIColor? { return UIColor(named: "primary") }
+}
